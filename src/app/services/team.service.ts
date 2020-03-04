05991d6fe6952b066e82c4b1d5d7ca49b7821368
Erik Kringstad Olsen <Erik.Kringstad.Olsen@computas.com>
2020-03-04 09:13:23 +0100

    Score submitted now updates score

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -16,4 +16,39 @@ export class TeamService {
   getAllTeams() {
     return this.firestore.collection<Team>('team').snapshotChanges();
   }
+
+  updateLength(name: string, task : number, length: number) {
+    var teamRef = this.firestore.collection("team").doc(name);
+    if(task == 1){
+      return teamRef.update({
+          task1length: length
+      })
+    }
+    if(task == 2){
+      return teamRef.update({
+          task2length: length
+      })
+    }
+    if(task == 3){
+      return teamRef.update({
+          task3length: length
+      })
+    }
+    if(task == 4){
+      return teamRef.update({
+          task4length: length
+      })
+    }
+    if(task == 5){
+      return teamRef.update({
+          task5length: length
+      })
+    }
+    if(task == 6){
+      return teamRef.update({
+          task1length: length
+      })
+    }
+    
+  }
 }
